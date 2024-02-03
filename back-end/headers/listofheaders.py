fc1ee0dae59be99a14ff56367efa13aa0e270d7f
Reynella Liebig <reynella.liebig@gmail.com>
2024-02-03 10:16:53 -0500

    changed names

--- back-end/headers/listofheaders.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-import additionals
-import listofingredients
-import listofinstructs
-
-class Headers(BaseModel):
-    ingredients: listofingredients
-    instructions: listofinstructs
-    additional: additionals
