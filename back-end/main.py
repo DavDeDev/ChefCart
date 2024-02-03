0900e2d52440b2d96e587c889017f074cfe54266
Reynella Liebig <reynella.liebig@gmail.com>
2024-02-03 09:05:49 -0500

    added JSON Response `#

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
+import recipe
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.put("recipe")
+def update_item(item: recipe):
+    json_compatible_item_data = jsonable_encoder(item)
+    return JSONResponse(content=json_compatible_item_data)
