e12384948dc697ac59b5ddf96f48c8620767265b
Reynella Liebig <reynella.liebig@gmail.com>
2024-02-03 21:13:45 -0500

    changed chat.py

--- back-end/chat.py
+++ back-end/chat.py
@@ -1,27 +1,36 @@
-import openai
-import os
-import sys
+# import openai
+# import os
+# import sys
 
-def api_key():
-    try:  
-        key = os.environ['API_KEY_CREDENTIALS']
-        return key
-    except KeyError: 
-        print('[error]: `API_KEY_CREDENTIALS` environment variable required')
-        sys.exit(1)
+# # def api_key():
+# #     try:  
+# #         key = os.environ['API_KEY_CREDENTIALS']
+# #         return key
+# #     except KeyError: 
+# #         print('[error]: `API_KEY_CREDENTIALS` environment variable required')
+# #         sys.exit(1)
 
-openai.api_key = api_key()
+# openai.api_key = "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"
 
-model_engine = "text-davinci-003"
-prompt = "Hi, how are you today?"
+# model_engine = "gpt-3.5-turbo"
+# prompt = "Hi, how are you today?"
 
-completion = openai.completions.create(
-    model=model_engine,
-    prompt=prompt,
-    n=1,
-    stop=None,
-    temperature=0.5,
-)
+# completion = openai.completions.create(
+#     model=model_engine,
+#     prompt=prompt,
+#     max_tokens=1024,
+#     n=1,
+#     stop=None,
+#     temperature=0.5,
+# )
 
-response = completion.choices[0].text
-print(response)
+# response = completion.choices[0].text
+# print(response)
+
+# from openai import OpenAI
+# client = OpenAI()
+
+# response = client.completions.create(
+#   model="gpt-3.5-turbo-0125",
+#   prompt="How are you today?"
+# )
